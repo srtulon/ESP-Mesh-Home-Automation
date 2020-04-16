8ba51a008eaaa9bb0bbf63fb222b8df6282f4069
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-04-16 13:24:59 +0600

    Fixed linked device conflict

--- script.py
+++ script.py
@@ -1,14 +1,12 @@
 import paho.mqtt.client as mqtt
 import re
 
-
 import mysql.connector as mariadb
 
-#Declaring global variable
-did = 0        #devie id
-dtype = 0      #device type
-dstatus = 0    #devie status
-
+# Declaring global variable
+did = 0  # devie id
+dtype = 0  # device type
+dstatus = 0  # devie status
 
 ########################## DATABASE PART ###########################################
 
@@ -25,9 +23,9 @@ c.execute("create database IF NOT EXISTS test")
 
 # create table
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS devices ( id varchar(20) not null, type varchar(20) not null,name varchar(20) not null, status int, unique (id))')
-    c.execute('CREATE TABLE IF NOT EXISTS stat_timeline (id varchar(20) not null, status int, TimeStamp TIMESTAMP)')
-    c.execute('CREATE TABLE IF NOT EXISTS links (id varchar(20) not null, link_id varchar(20) , link int)')
+    c.execute('CREATE TABLE IF NOT EXISTS devices ( id varchar(20) not null, type varchar(20) not null,name varchar(20) not null, status int, PRIMARY KEY (id))')
+    c.execute('CREATE TABLE IF NOT EXISTS stat_timeline (id varchar(20) not null, status int, TimeStamp TIMESTAMP, FOREIGN KEY (id) REFERENCES devices(id))')
+    c.execute('CREATE TABLE IF NOT EXISTS links (id varchar(20) not null, link_id varchar(20) , link int, FOREIGN KEY (id) REFERENCES devices(id))')
 
 
 # data entry
@@ -91,20 +89,39 @@ def link():
         print("Error: {}".format(error))
     for row in data:
         # update status change in stat_timeline and devices
-        try:
-            print(row[0])
-            # update status change in stat_timeline and devices
-            c.execute('INSERT INTO stat_timeline (id,status) VALUES (%s,%s);', (row[0], dstatus))
-            c.execute('UPDATE devices SET status = ' + dstatus + ' WHERE id =' + f"{row[0]}")
-            # send linked device status via MQTT [Format : @(status)%)]
-            temp=row[0].split('.')
-            ori_id=temp[0]
-            relay_num=temp[1]
-            send_message(ori_id, '@' + relay_num + dstatus + '%')
-            #send_message(f"{row[0]}", '@' + dstatus + '%')
-        except mariadb.Error as error:
-            print("Error: {}".format(error))
+        # if any sensor is high then the device status is set to high
+        if (dstatus=='1'):
+            try:
+                print(row[0])
+                # update status change in stat_timeline and devices
+                set_status(device_id=row[0], status='1')
+            except mariadb.Error as error:
+                print("Error: {}".format(error))
+                
+        elif (dstatus=='0'):
+            try:
+                print(row[0])
+                c.execute('SELECT d.* FROM devices d INNER JOIN links l ON d.id = l.id WHERE l.link_id=%s AND  d.status=1;',(row[0]))
+                data = c.fetchall()
+                print(data)
 
+                if not data==None:
+                    # update status change in stat_timeline and devices
+                    set_status(device_id=row[0], status='1')
+                else:
+                    # update status change in stat_timeline and devices
+                    set_status(device_id=row[0], status='0')
+            except mariadb.Error as error:
+                print("Error: {}".format(error))
+
+def set_status(device_id,status):
+    c.execute('INSERT INTO stat_timeline (id,status) VALUES (%s,%s);', (device_id, status))
+    c.execute('UPDATE devices SET status = ' + status + ' WHERE id =' + f"{device_id}")
+    # send linked device status via MQTT [Format : @(relay number)(status)%)]
+    temp = device_id.split('.')
+    ori_id = temp[0]
+    relay_num = temp[1]
+    send_message(ori_id, '@' + relay_num + status + '%')
 
 conn.commit()
 
@@ -149,9 +166,7 @@ def on_message(client, userdata, msg):
         # message format: #(id),(type),(status)$
         if txt[0] == '#' and txt.find('$') > 0:
             txt = txt.replace("#", "")
-            # txt=txt.replace("$","")
-            # print(msg.topic+" "+txt)
-            t = txt.split("$")
+            t = txt.split("$") # to discard garbage values after '$'   
             # print(t[0])
             s = t[0].split(",")
             did = s[0]
@@ -159,7 +174,7 @@ def on_message(client, userdata, msg):
             dstatus = s[2]
             # print(len(did))
             # print(did)
-            #print(dtype)
+            # print(dtype)
             print(dstatus)
             # data_entry(did,dtype,did,dstatus)
 
@@ -178,7 +193,6 @@ def send_message(dev, msg):
     # print(dev)
     client.publish(dev, msg)
     # print("send check")
-
 
 
 client = mqtt.Client()

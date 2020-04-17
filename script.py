fa3490a7f22d6485f5499c4601af1a8ad51d4fbc
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-04-17 16:15:46 +0600

    Optimized data_entry() function

--- script.py
+++ script.py
@@ -54,7 +54,7 @@ def data_entry():
 
     # insert new data in stat_timeline if device status is changed
     try:
-        c.execute('SELECT status FROM stat_timeline WHERE id=' + did + ' ORDER BY `timestamp`  DESC limit 1')
+        c.execute('SELECT status FROM devices WHERE id=' + did)
     except mariadb.Error as error:
         print("Error: {}".format(error))
 
@@ -65,13 +65,14 @@ def data_entry():
         # print(data1)
         # print(data2)
 
+        # matching current status with previous to avoid multiple entry
         if data1 == data2 and row is not None:
             print("Matched")
         else:
             print("Not matched")
             try:
-                c.execute('INSERT INTO stat_timeline (id,status) VALUES (%s,%s);', (did, dstatus))
-                c.execute('UPDATE devices SET status = ' + dstatus + ' WHERE id =' + did)
+                # for sensors, no need to send status
+                set_status(device_id=did, status=dstatus, send=False)
                 link()
             except mariadb.Error as error:
                 print("Error: {}".format(error))
@@ -82,7 +83,6 @@ def link():
     # select linked devices
     try:
         c.execute('SELECT link_id FROM links WHERE id=' + did + ' AND link= 1;')
-        # print("check")
         data = c.fetchall()
         # print(data)
     except mariadb.Error as error:
@@ -105,24 +105,28 @@ def link():
 
                 if len(data) == 0:
                     # update status change in stat_timeline and devices
-                    set_status(device_id=row[0], status='0')
+                    set_status(device_id=row[0], status='0',send=True)
                 else:
                     # update status change in stat_timeline and devices
-                    set_status(device_id=row[0], status='1')
+                    set_status(device_id=row[0], status='1',send=True)
             except mariadb.Error as error:
                 print("Error: {}".format(error))
 
-def set_status(device_id,status):
+def set_status(device_id,status,send):
     try:
         c.execute('INSERT INTO stat_timeline (id,status) VALUES (%s,%s);', (device_id, status))
+    except mariadb.Error as error:
+        print("Error: {}".format(error))
+    try:
         c.execute('UPDATE devices SET status = ' + status + ' WHERE id =' + f"{device_id}")
+    except mariadb.Error as error:
+        print("Error: {}".format(error))
+    if send:
         # send linked device status via MQTT [Format : @(relay number)(status)%)]
         temp = device_id.split('.')
         ori_id = temp[0]
         relay_num = temp[1]
         send_message(ori_id, '@' + relay_num + status + '%')
-    except mariadb.Error as error:
-        print("Error: {}".format(error))
 
 conn.commit()
 
@@ -175,7 +179,6 @@ def on_message(client, userdata, msg):
             # print(did)
             # print(dtype)
             print(dstatus)
-            # data_entry(did,dtype,did,dstatus)
 
             # for ignoring garbage data
             if len(did) == 9 or len(did) == 10 or len(did) == 11:
@@ -197,5 +200,5 @@ def send_message(dev, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("MQTT.com", 1883, 60)   #change the address to MQTT broker server
+client.connect("MQTT.com", 1883, 60)  # change the address to MQTT broker server
 client.loop_forever()

3f5d3a159e4bedd07aa2ac95ba206a99aac6dc47
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-11-16 23:09:42 +0600

    Update serial_test_2.ino

--- Arduino_test_files/serial_test_2/serial_test_2.ino
+++ Arduino_test_files/serial_test_2/serial_test_2.ino
@@ -1,12 +1,19 @@
 void setup() {
-Serial.begin(9600); // set the baud rate
-Serial.println("Ready"); // print "Ready" once
+  Serial.begin(115200); // set the baud rate
+  Serial.println("Ready"); // print "Ready" once
 }
 void loop() {
-char inByte = ' ';
-if(Serial.available()){ // only send data back if data has been sent
-char inByte = Serial.read(); // read the incoming data
-Serial.println(inByte); // send the data back in a new line so that it is not all one long line
-}
-delay(100); // delay for 1/10 of a second
+  if(Serial.available()){ // only send data back if data has been sent
+    String str = Serial.readString(); // read the incoming data
+    //Serial.println(str);
+    if(str[0]=='i'){
+      Serial.println("ip: "+str.substring(1, str.length())); // send the data back in a new line so that it is not all one long line
+    }
+    else if(str[0]=='w'){
+      Serial.println("wifi: "+str.substring(1, str.length())); // send the data back in a new line so that it is not all one long line
+    }
+    else if(str[0]=='p'){
+      Serial.println("password: "+str.substring(1, str.length())); // send the data back in a new line so that it is not all one long line
+    }
+  } 
 }

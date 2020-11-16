7b2eb073f2beaad3e245a6fbe6a08b8ada8487fb
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-11-16 20:45:01 +0600

    Create serial_test_3.py

---
+++ Python_test_scripts/serial_test_3.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+# Define the serial port and baud rate.
+# Ensure the 'COM#' corresponds to what was seen in the Windows Device Manager
+ser = serial.Serial('COM7', 9600)
+counter = 32 # Below 32 everything in ASCII is gibberish
+
+while True:
+     counter +=1
+     ser.write(str.encode(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
+     print (ser.readline().strip().decode( "utf-8" )) # Read the newest output from the Arduino
+     print(counter)
+     time.sleep(.1) # Delay for one tenth of a second
+     if counter == 255:
+       counter = 32

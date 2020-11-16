b6f75c2a1c1a15ade90331f666d7217cdd538194
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-11-16 23:09:54 +0600

    Update serial_test_3.ino

--- Arduino_test_files/serial_test_3/serial_test_3.ino
+++ Arduino_test_files/serial_test_3/serial_test_3.ino
@@ -1,12 +1,150 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library to
+// connect to a another network and relay messages from a MQTT broker to the nodes of the mesh network.
+// To send a message to a mesh node, you can publish it to "device/to/12345678" where 12345678 equals the nodeId.
+// To broadcast a message to all nodes in the mesh you can publish it to "device/to/broadcast".
+// When you publish "getNodes" to "device/to/gateway" you receive the mesh topology as JSON
+// Every message from the mesh which is send to the gateway node will be published to "device/from/12345678" where 12345678 
+// is the nodeId from which the packet was send.
+//************************************************************
+
+#include <Arduino.h>
+#include <painlessMesh.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+
+#define   MESH_PREFIX     "mesh"
+#define   MESH_PASSWORD   "password"
+#define   MESH_PORT       5555
+
+#define   STATION_SSID     "xaneon"
+#define   STATION_PASSWORD "bmwm3gtr"
+
+#define HOSTNAME "MQTT_Bridge"
+
+// Prototypes
+void receivedCallback( const uint32_t &from, const String &msg );
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+
+IPAddress getlocalIP();
+
+IPAddress myIP(0,0,0,0);
+
+int a=192;
+int b=168;
+int c=0;
+int d=102;
+IPAddress mqttBroker(a, b, c, d);
+int mqttport=1883;
+
+//const char* mqttBroker = "raspberrypi.local";
+/*
+const char* mqttuser = "vvjqiinu";
+const char* mqttpassword = "jemMAT3tJ8xf";
+*/
+
+
+
+painlessMesh  mesh;
+
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(mqttBroker, 1883, mqttCallback, wifiClient);
+//PubSubClient mqttClient(wifiClient);
+
 void setup() {
-Serial.begin(9600); // set the baud rate
-Serial.println("Ready"); // print "Ready" once
+  Serial.begin(115200);
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+
+  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
+  // network (STATION_SSID)
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
+  mesh.onReceive(&receivedCallback);
+
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  mesh.setHostname(HOSTNAME);
+
+  // Bridge node, should (in most cases) be a root node. See [the wiki](https://gitlab.com/device/device/wikis/Possible-challenges-in-mesh-formation) for some background
+  mesh.setRoot(true);
+  // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
+  mesh.setContainsRoot(true);
+  mqttClient.setServer(mqttBroker, mqttport);
+  mqttClient.setCallback(mqttCallback);
 }
+
 void loop() {
-char inByte = ' ';
-if(Serial.available()){ // only send data back if data has been sent
-char inByte = Serial.read(); // read the incoming data
-Serial.println(inByte); // send the data back in a new line so that it is not all one long line
+  mesh.update();
+  mqttClient.loop();
+
+  if(myIP != getlocalIP()){
+    myIP = getlocalIP();
+    Serial.println("My IP is " + myIP.toString());
+  }
+  if (!mqttClient.connected() && myIP == getlocalIP()){
+      Serial.println("Attempting MQTT connection...");
+      if(mqttClient.connect("painlessMeshClient")){
+        Serial.println("connected");
+        mqttClient.publish("device/from/gateway","Ready!");
+        mqttClient.subscribe("device/to/#");
+    }
+  }
 }
-delay(100); // delay for 1/10 of a second
+
+
+void receivedCallback( const uint32_t &from, const String &msg ) {
+  Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+  String topic = "device/from/" + String(from);
+  mqttClient.publish(topic.c_str(), msg.c_str());
+}
+
+void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
+  char* cleanPayload = (char*)malloc(length+1);
+  payload[length] = '\0';
+  memcpy(cleanPayload, payload, length+1);
+  String msg = String(cleanPayload);
+  free(cleanPayload);
+
+  String targetStr = String(topic).substring(10);
+  Serial.println(msg);
+  Serial.println(targetStr);
+  
+  if(targetStr == "gateway")
+  {
+    if(msg == "getNodes")
+    {
+      auto nodes = mesh.getNodeList(true);
+      String str;
+      for (auto &&id : nodes)
+        str += String(id) + String(" ");
+      mqttClient.publish("device/from/gateway", str.c_str());
+    }
+  }
+  else if(targetStr == "broadcast") 
+  {
+    Serial.println("Broadcast");
+    mesh.sendBroadcast(msg);
+  }
+  else
+  {
+    uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
+    Serial.printf("Target:");
+    Serial.println(target);
+    mesh.sendSingle(target, msg);
+
+    //removed client checking because it is not working
+    /*
+    if(mesh.isConnected(target))
+    {
+      mesh.sendSingle(target, msg);
+    }
+    else
+    {
+      mqttClient.publish("device/from/gateway", "Client not connected!");
+    }
+    */
+  }
+}
+
+IPAddress getlocalIP() {
+  return IPAddress(mesh.getStationIP());
 }

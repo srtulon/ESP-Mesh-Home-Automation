9bf6b769e7ca1d5e7021e72ff90c912486c773ca
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-11-16 23:09:45 +0600

    Update serial_test_3.py

--- Python_test_scripts/serial_test_3.py
+++ Python_test_scripts/serial_test_3.py
@@ -1,16 +1,36 @@
 import serial
 import time
-
+import socket
+## getting the hostname by socket.gethostname() method
+hostname = socket.gethostname()
+## getting the IP address using socket.gethostbyname() method
+ip_address = socket.gethostbyname(hostname)
 # Define the serial port and baud rate.
 # Ensure the 'COM#' corresponds to what was seen in the Windows Device Manager
-ser = serial.Serial('COM7', 9600)
-counter = 32 # Below 32 everything in ASCII is gibberish
+ser = serial.Serial('COM7', 115200)
 
-while True:
-     counter +=1
-     ser.write(str.encode(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
-     print (ser.readline().strip().decode( "utf-8" )) # Read the newest output from the Arduino
-     print(counter)
-     time.sleep(.1) # Delay for one tenth of a second
-     if counter == 255:
-       counter = 32
+## getting the hostname by socket.gethostname() method
+hostname = socket.gethostname()
+## getting the IP address using socket.gethostbyname() method
+ip = socket.gethostbyname(hostname)
+ip='i'+ip
+ip=bytes(ip, 'utf-8')
+wifi=b"wxaneon"
+password=b"pbmwm3gtr"
+
+def setup(counter):
+    if counter == 1:
+        ser.write(ip) # Convert the decimal number to ASCII then send it to the Arduino
+        print (ser.readline().strip().decode( "utf-8" )) # Read the newest output from the Arduino
+    ser.write(ip) # Convert the decimal number to ASCII then send it to the Arduino
+    print (ser.readline().strip().decode( "utf-8" )) # Read the newest output from the Arduino
+    time.sleep(1) # Delay for one tenth of a second
+    ser.write(wifi) # Convert the decimal number to ASCII then send it to the Arduino
+    print (ser.readline().strip().decode( "utf-8" )) # Read the newest output from the Arduino
+    time.sleep(1) # Delay for one tenth of a second
+    ser.write(password) # Convert the decimal number to ASCII then send it to the Arduino
+    print (ser.readline().strip().decode( "utf-8" )) # Read the newest output from the Arduino
+    time.sleep(1) # Delay for one tenth of a second
+    ser.close()
+
+setup(1)

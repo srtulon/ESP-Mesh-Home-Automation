d274ad0d7d02dff87f26a1b1732aa104155e566f
Sadid Rafsun Tulon <srtulon6@gmail.com>
2020-11-21 22:46:47 +0600

    Update ac_protocol_names_fetch.py

--- Python_test_scripts/ac_protocol_names_fetch.py
+++ Python_test_scripts/ac_protocol_names_fetch.py
@@ -7,4 +7,8 @@ count=0
 for x in contents:
      x.rstrip()
      count=count+1
-     print(str(count)+'.'+x.strip())
+     #print(str(count)+'.'+x.strip())
+     print("    case \'"+str(count)+"\':")
+     print("        Serial.print(\"Protocol: "+x.strip()+"\");")
+     print("        ac.next.protocol = decode_type_t::"+x.strip()+";")
+     print("        break;")

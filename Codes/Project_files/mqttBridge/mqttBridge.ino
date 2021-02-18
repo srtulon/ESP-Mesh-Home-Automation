1d8fab028d2d02fdda8a59fb27101bcefb31bf08
Sadid Rafsun Tulon <srtulon6@gmail.com>
2021-02-18 18:53:59 +0600

    Update mqttBridge.ino

--- Codes/Project_files/mqttBridge/mqttBridge.ino
+++ Codes/Project_files/mqttBridge/mqttBridge.ino
@@ -17,11 +17,11 @@
 #define   MESH_PASSWORD   "password"
 #define   MESH_PORT       5555
 
-//#define   STATION_SSID     "xaneon"
-//#define   STATION_PASSWORD "bmwm3gtr"
+#define   STATION_SSID     "xaneon"
+#define   STATION_PASSWORD "bmwm3gtr"
 
-#define   STATION_SSID     "IOI wifi"
-#define   STATION_PASSWORD "IoI2021@wifi"
+//#define   STATION_SSID     "IOI wifi"
+//#define   STATION_PASSWORD "IoI2021@wifi"
 
 #define HOSTNAME "MQTT_Bridge"
 
@@ -35,8 +35,8 @@ IPAddress myIP(0,0,0,0);
 
 int a=192;
 int b=168;
-int c=1;
-int d=25;
+int c=0;
+int d=102;
 IPAddress mqttBroker(a, b, c, d);
 int mqttport=1883;
 

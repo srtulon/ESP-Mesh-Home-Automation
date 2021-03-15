f630ffc8c91f17c897560825dee77a9f3242bb6a
Sadid Rafsun Tulon <srtulon6@gmail.com>
2021-03-16 04:57:43 +0600

    Update control_demo.py

--- Codes/Project_files/Python_scripts_database/control_demo.py
+++ Codes/Project_files/Python_scripts_database/control_demo.py
@@ -22,11 +22,20 @@ link1 = [r for r, in r_set] # create a  list
 
 link2 = link1 # create a  list
 
+query="SELECT protocol FROM ac_list"
+r_set=my_conn.execute(query);
+link3 = [r for r, in r_set] # create a  list
 
+query="SELECT id FROM acs"
+r_set=my_conn.execute(query);
+link4 = [r for r, in r_set] # create a  list
+
+templist=[16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
 zero_one1=[0,1]
 zero_one2=[0,1]
 zero_one3=[0,1]
+zero_one4=[0,1]
 add_delete=['Add','Del']
 
 def function1():
@@ -36,10 +45,10 @@ def function1():
     elif(options6.get()=='Del'):
         s='rlr'
 
-    str='^'+s+','+options1.get()+','+options2.get()+','+options3.get()+','+options4.get()+'!'
+    s='^'+s+','+options1.get()+','+options2.get()+','+options3.get()+','+options4.get()+'!'
 
-    print(str)
-    publish.single( "device/from", str, hostname="192.168.1.28")
+    print(s)
+    publish.single( "device/from", s, hostname="192.168.1.18")
 
 
 def function2():
@@ -49,14 +58,31 @@ def function2():
     print(ori_id)
     relay_num = temp[1]
     status=options8.get()
-    str='@' + relay_num + status + '%'
-    print(str)
-    publish.single( "device/to/"+ori_id, str, hostname="192.168.1.28")
+    s='@' + relay_num + status + '%'
+    print(s)
+    publish.single( "device/to/"+ori_id, s, hostname="192.168.1.18")
+
+
+def function3():
+
+    id = options12.get()
+    print(id)
+    prot=link3.index(options9.get())
+    if prot<10:
+        prot1='0'+str(prot)
+    else:
+        prot1=str(prot)
+    pow=options10.get()
+    tem=options11.get()
+    s='@'+prot1+'1'+pow+tem+'%'
+    print(s)
+    publish.single( "device/to/"+id, s, hostname="192.168.1.18")
+
 
 
 import tkinter as tk
 my_w = tk.Tk()
-my_w.geometry("600x500")  # Size of the window 
+my_w.geometry("700x600")  # Size of the window 
 my_w.title("App")  # Adding a title
 
 options1 = tk.StringVar(my_w)
@@ -76,28 +102,28 @@ options6.set(add_delete[0]) # default value
 
 
 om6 =tk.OptionMenu(my_w, options6, *add_delete)
-tk.Label(my_w, text="Option").grid(row = 1, column = 1)
-om6.grid(row=2,column=1)
+#tk.Label(my_w, text="Option").grid(row = 1, column = 1)
+#om6.grid(row=2,column=1)
 
 om1 =tk.OptionMenu(my_w, options1, *id)
-tk.Label(my_w, text="Pir").grid(row = 1, column = 2)
-om1.grid(row=2,column=2)
+#tk.Label(my_w, text="Pir").grid(row = 1, column = 2)
+#om1.grid(row=2,column=2)
 
 
 om2 =tk.OptionMenu(my_w, options2, *link1)
-tk.Label(my_w, text="Relay").grid(row = 1, column = 3)
-om2.grid(row=2,column=3)
+#tk.Label(my_w, text="Relay").grid(row = 1, column = 3)
+#om2.grid(row=2,column=3)
 
 om3 =tk.OptionMenu(my_w, options3, *zero_one1)
-tk.Label(my_w, text="Link").grid(row = 1, column = 4)
-om3.grid(row=2,column=4)
+#tk.Label(my_w, text="Link").grid(row = 1, column = 4)
+#om3.grid(row=2,column=4)
 
 om4 =tk.OptionMenu(my_w, options4, *zero_one2)
-tk.Label(my_w, text="Priority").grid(row = 1, column = 5)
-om4.grid(row=2,column=5)
+#tk.Label(my_w, text="Priority").grid(row = 1, column = 5)
+#om4.grid(row=2,column=5)
 
 button1 = tk.Button(my_w, text='Send', width=15, command=function1)
-button1.grid(row=2,column=7)
+#button1.grid(row=2,column=7)
 
 
 
@@ -109,15 +135,50 @@ options8 = tk.StringVar(my_w)
 options8.set(zero_one3[1]) # default value
 
 om7 =tk.OptionMenu(my_w, options7, *link2)
-tk.Label(my_w, text="Id").grid(row = 4, column = 1)
+tk.Label(my_w, text="Name").grid(row = 4, column = 1)
 om7.grid(row=5,column=1)
 
 om8 =tk.OptionMenu(my_w, options8, *zero_one3)
-tk.Label(my_w, text="Com").grid(row = 4, column = 2)
+tk.Label(my_w, text="Command").grid(row = 4, column = 2)
 om8.grid(row=5,column=2)
 
 button2 = tk.Button(my_w, text='Send', width=15, command=function2)
 button2.grid(row=5,column=3)
+
+
+######## ac control #########
+options12 = tk.StringVar(my_w)
+options12.set(link4[0]) # default value
+
+om12 =tk.OptionMenu(my_w, options12, *link4)
+tk.Label(my_w, text="Name").grid(row = 6, column = 1)
+om12.grid(row=7,column=1)
+
+options9 = tk.StringVar(my_w)
+options9.set(link3[0]) # default value
+
+options10 = tk.StringVar(my_w)
+options10.set(zero_one3[1]) # default value
+
+om9 =tk.OptionMenu(my_w, options9, *link3)
+tk.Label(my_w, text="Company").grid(row = 6, column = 2)
+om9.grid(row=7,column=2)
+
+om10 =tk.OptionMenu(my_w, options10, *zero_one4)
+tk.Label(my_w, text="Power").grid(row = 6, column = 3)
+om10.grid(row=7,column=3)
+
+options11 = tk.StringVar(my_w)
+options11.set(templist[1]) # default value
+
+om11 =tk.OptionMenu(my_w, options11, *templist)
+tk.Label(my_w, text="Temperature").grid(row = 6, column = 4)
+om11.grid(row=7,column=4)
+
+
+button3 = tk.Button(my_w, text='Send', width=15, command=function3)
+button3.grid(row=7,column=5)
+
 
 
 def degis(v,c):
